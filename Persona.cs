3cb5c6b565c03ade128d408334ed303be4f18b30
Gabriele Viganò <gabriviga2005@gmail.com>
2024-01-30 19:18:55 +0100

    Aggiungere i file di progetto.

---
+++ Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatiStessoGiorno
+{
+    internal class Persona
+    {
+        public Persona() { }
+        public int mese;
+        public int giorno;
+        public int anno;
+        public void GeneraDataNascita(Random random) 
+        {
+            bool stop = false;
+            while (stop == false)
+            {
+                mese = random.Next(1, 13);
+                giorno = random.Next(1, 32);
+                anno = random.Next(0, 2024);
+                if(giorno <= DateTime.DaysInMonth(anno, mese))
+                {
+                    stop = true;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatiStessoGiorno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List <Persona> persone = new List <Persona>();
+            List <double> percentuali = new List <double>();
+            int casi = 0;
+            int prove = 100;
+
+            for (int i = 0; i < 366; i++)
+            {
+                casi = 0;
+                for(int j = 0; j < prove; j++)
+                {
+                    CompilaLista(random, i, persone);
+                    casi += ConfrontaCompleanni(persone);
+                }
+                percentuali[i] = casi / prove;
+            }
+        }
+        static void CompilaLista(Random random, int nPersone, List <Persona> persone)
+        {
+            for(int i = 0; i < nPersone; i++)
+            {
+                Persona p = new Persona();
+                p.GeneraDataNascita(random);
+                persone.Add(p);
+            }
+        }
+        static int ConfrontaCompleanni(List <Persona> persone)
+        {
+            return 0;
+        }
+    }
+}
